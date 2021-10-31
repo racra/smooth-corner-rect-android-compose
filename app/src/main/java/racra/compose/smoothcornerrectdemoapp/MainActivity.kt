64d4723cc7be090fde494b63773a565a5e2cd856
Rodrigo Alves <rodrigodevs@gmail.com>
2021-10-31 16:35:38 +0000

    Added demo app UI

--- app/src/main/java/racra/compose/smoothcornerrectdemoapp/MainActivity.kt
+++ app/src/main/java/racra/compose/smoothcornerrectdemoapp/MainActivity.kt
@@ -3,16 +3,23 @@ package racra.compose.smoothcornerrectdemoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import racra.compose.smooth_corner_rect_library.AbsoluteSmoothCornerShape
 import racra.compose.smoothcornerrectdemoapp.ui.theme.SmoothCornerRectDemoAppTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,8 +32,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoApp() {
+    var radius by remember {
+        mutableStateOf(50f)
+    }
+    var smoothness by remember {
+        mutableStateOf(50f)
+    }
+
     SmoothCornerRectDemoAppTheme {
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(60.dp, 0.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Surface(
                 modifier = Modifier
                     .padding(25.dp)
@@ -41,8 +61,20 @@ fun DemoApp() {
                     .height(200.dp)
                     .width(200.dp),
                 color = Color(0xFFF6EABE),
-                shape = RoundedCornerShape(50.dp)
+                shape = AbsoluteSmoothCornerShape(Dp(radius), smoothness.toInt())
             ) {}
+            Text(text = "Radius: ${radius.toInt()}")
+            Slider(
+                value = radius,
+                onValueChange = { radius = it },
+                valueRange = 0f..100f
+            )
+            Text(text = "Smoothness: ${smoothness.toInt()}")
+            Slider(
+                value = smoothness,
+                onValueChange = { smoothness = it },
+                valueRange = 0f..100f
+            )
         }
     }
 }
--- app/src/main/java/racra/compose/smoothcornerrectdemoapp/ui/theme/Theme.kt
+++ app/src/main/java/racra/compose/smoothcornerrectdemoapp/ui/theme/Theme.kt
@@ -1,16 +1,8 @@
 package racra.compose.smoothcornerrectdemoapp.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
@@ -29,17 +21,10 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun SmoothCornerRectDemoAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
