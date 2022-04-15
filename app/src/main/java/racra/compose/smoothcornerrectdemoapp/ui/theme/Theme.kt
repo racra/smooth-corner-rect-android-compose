c64a559d047746182f29be346e469098159e3374
racra <rodrigodevs@gmail.com>
2022-04-15 16:15:52 +0100

    Updated demo app styling

--- app/src/main/java/racra/compose/smoothcornerrectdemoapp/ui/theme/Theme.kt
+++ app/src/main/java/racra/compose/smoothcornerrectdemoapp/ui/theme/Theme.kt
@@ -3,9 +3,10 @@ package racra.compose.smoothcornerrectdemoapp.ui.theme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = Color(0xffB7CADB),
     primaryVariant = Purple700,
     secondary = Teal200
 
